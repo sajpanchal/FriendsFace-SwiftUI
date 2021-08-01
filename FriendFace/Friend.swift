9ed5aa84fc15a9fd0274fc18aaf240c8b36177e2
Saj Panchal <sajpanchal@gmail.com>
2021-08-01 12:33:02 -0400

    created User and Friend Structs and Created a Class to hold array of users that is persistant throughout the app.

---
+++ FriendFace/Friend.swift
@@ -0,0 +1,12 @@
+//
+//  Friend.swift
+//  FriendFace
+//
+//  Created by saj panchal on 2021-08-01.
+//
+
+import Foundation
+struct Friend: Codable {
+    var id: String
+    var name: String
+}
---
+++ FriendFace/User.swift
@@ -0,0 +1,25 @@
+//
+//  User.swift
+//  FriendFace
+//
+//  Created by saj panchal on 2021-08-01.
+//
+
+import Foundation
+struct User: Codable {
+    var id: String
+    var isActive: Bool
+    var name: String
+    var age: Int
+    var company: String
+    var email: String
+    var address: String
+    var about: String
+    var registered: String
+    var friends: [Friend]
+}
+
+class Users: ObservableObject {
+    @Published var users: [User]?
+    
+}
